4974e6a15a51c5e046e2b598dd568483dfd1c061
Kavya Gopal <95kavya@gmail.com>
2022-09-30 14:42:47 -0700

    Time: 28 ms (82.53%), Space: 46.2 MB (97.98%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,29 +1,31 @@
 class Solution {
     
-    public void twoSum(int[] nums, int index, List<List<Integer>> result) { 
-        int left = index + 1, right = nums.length - 1;
+    public void twoSum(List<List<Integer>> result, int i, int[] nums) {
+        int left = i+1, right = nums.length - 1;
         while (left < right) {
-            int sum = nums[index] + nums[left] + nums[right];
+            int sum = nums[i] + nums[left] + nums[right];
             if (sum == 0) {
-                result.add(Arrays.asList(nums[index], nums[left], nums[right]));
+                result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                 left++;
                 right--;
-                while(left < right && nums[left] == nums[left - 1])
+                while(left < nums.length && nums[left] == nums[left-1] ) {
                     left++;
-            } else if ( sum < 0) {
+                }
+                    
+            } else if (sum < 0) {
                 left++;
             } else {
                 right--;
             }
         }
-        
     }
+    
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
-            if ( i ==0 || nums[i-1] != nums[i]) {
-                twoSum(nums, i, result);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 ||  nums[i] != nums[i-1]) {
+                twoSum(result, i, nums);
             }
         }
         return result;
